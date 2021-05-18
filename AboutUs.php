d1aaaa8f6e5501a7658c994e11d132700541bae0
aishwarya-palimkar <aspalimkar2018@gmail.com>
2021-05-18 23:35:51 +0530

    first commit

---
+++ AboutUs.php
@@ -0,0 +1,81 @@
+<?php
+include 'connection.php';?>
+<html>
+<head>
+    <title>
+        Sealed safe Bank
+    </title>
+ <!-- Latest compiled and minified CSS -->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+ <!-- jQuery library -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ 
+ <!-- Latest compiled JavaScript -->
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link rel="stylesheet"  type="text/css" href="style.css">
+ <style>
+     h1,h4{
+         color: white;
+     }
+     </style>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <ul class="nav navbar-nav navbar-left">
+                    <h4><img src="download.jpg"alt="bank"width="40px" height="40px"><b> Sealed Safe Bank</b></h4></a>
+                    </ul>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="customers.php" target="_blank"> <span class="glyphicon glyphicon-user"></span>Customers</a></li>
+                        <li><a href="transaction.php"> <span class="glyphicon glyphicon-time"></span>Transaction History </a></li> 
+                        <li><a href="bank.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav> <br><br><br>
+        <div class="back_image">
+    <div class="container">
+        <div class="jumbotron">
+        <h1>SEALED SAFE BANK <img src="download.jpg"alt="bank"width="100px" height="100px"></h1>
+        <h2>Financial services company</h2><br>
+        <p>Sealed Safe Bank is an Indian 
+             public sector banking and financial services statutory body headquartered in Delhi, Maharashtra.<a href="https://en.wikipedia.org/wiki/Bank" target="_blank">Wikipedia</a></p><br>
+             <p><b>Branch:</b> Sealed Safe Bank,Nanded Branch</p><br>
+             <p><b>Customer Service:</b> 1800 567 890</p><br>
+             <p><b>Headquarters:</b> Nanded </p><br>
+             <p><b>Chairperson:</b> Niharika Jain</p><br>
+             <p><b>Founded:</b>1 May 2021 </p><br>
+             <p><b>Stock price:</b> SSB (NSE) ₹360.60 -7.10 (-1.93%) </p><br>
+             <p><b>Subsidiaries:</b>SSB Card, Sealed Safe Bank (US) Limited, MORE </p><br>
+        </div>
+    </div>
+        </div>
+<footer>
+          <centre>
+            <div class="contain">
+                <div class="container">
+              <h3 style="text-align: center"><b>FIND US ON SOCIAL MEDIA</b></h3>
+              <center>
+              <img src="download.png"alt="google"width="40px" height="40px">
+              <img src="download (1).png"alt="twitter"width="40px" height="40px" style="text-align: center">
+              <img src="download (2).png"alt="linkedin"width="40px" height="40px" style="text-align: center">
+              <img src="download (3).png"alt="facebook"width="40px" height="40px" style="text-align: center"><br>
+              </center>
+              <p><b>Created by:Aishwarya Sudhir Palimkar</b></p>
+              <p style="text-align:center">“Copyright © Sealed Safe Bank. All Rights Reserved | Contact Us: +91 90000 00000”.</p>
+                 </div>
+            </div>                 
+              
+          </centre>
+      </footer>  
+</body>
+</html>
